790e10eaa80da03e69d45daa82068833b4eb6167
Ethan Cave <82524265+EthanCave@users.noreply.github.com>
2026-07-07 18:56:41 -0400

    Add: plus-one - submission-9

---
+++ Data Structures & Algorithms/plus-one/submission-9.py
@@ -0,0 +1,12 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits[-1] += 1
+
+        current_digit = len(digits) - 1
+        while digits[current_digit] == 10:
+            digits[current_digit] = 0
+            if current_digit == 0:
+                return [1] + digits
+            current_digit -= 1
+            digits[current_digit] += 1
+        return digits
